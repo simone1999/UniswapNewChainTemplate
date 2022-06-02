ac0c9727d4e9b05a1beea0907611600d4463fa51
Simon Tower <simon@klein-homepage.de>
2022-06-02 14:16:26 +0200

    found some places where init code hash and factory address were not replaced

--- forks/@uniswap/sdk/dist/sdk.esm.js
+++ forks/@uniswap/sdk/dist/sdk.esm.js
@@ -39,8 +39,8 @@ var Rounding;
   Rounding[Rounding["ROUND_UP"] = 2] = "ROUND_UP";
 })(Rounding || (Rounding = {}));
 
-var FACTORY_ADDRESS = '0x6Bd5A1A63ffF10De3c6B7C667040E9AE1B47fDf2';
-var INIT_CODE_HASH = '0xaf88dd15a55596feb9d67243c727bfd6144af12453963809bc91f0cfcf8241bc';
+var FACTORY_ADDRESS = '<factory_address>';
+var INIT_CODE_HASH = '<init_code_hash>';
 var MINIMUM_LIQUIDITY = /*#__PURE__*/JSBI.BigInt(1000); // exports for internal consumption
 
 var ZERO = /*#__PURE__*/JSBI.BigInt(0);
